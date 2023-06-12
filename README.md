9b75b2aab5562e9868fef1eff2f72d82c6e6e6c3
jacksonPrimo <jackson.primo12@gmail.com>
2023-06-11 22:30:09 -0300

    undo for old version

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-## Descrição
-Este é meu portifólio pessoal
+# Portfolio
+Este é um portfolio pessoal feito com o intuito de treinar meus conhecimentos em html e css puros sem auxilio de frameworks
+## Autor
+* jackson A. Primo `<jackson.primo12@gmail.com>`
+## Como utilizar
+para executar o projeto não é necessario fazer instalação alguma, apenas abrir o arquivo index.html no navegador, o projeto não faz uso de arquivos css pré processados tal como sass, nem bibiotecas para criação de componentes tal como React.
+## Como dar continuidade ao projeto
+Para iniciar o desenvolvimento, é necessário clonar o projeto do GitHub num diretório de sua preferência:
+```shell
+cd "diretorio de sua preferencia"
+git clone https://github.com/jacksonPrimo/portfolio.git
+```
+## Recursos
+O unico objetivo deste projeto é navegar por um portfolio pessoal por meio de links que redirecionam a locais especificos da pagina
+Possui um unico arquivo para html, css e js
+o arquivo js possui unicamete a função de dar destaque aos topicos do menu no topo da pagina baseado em sua posição vertical e faz uso unicamente de Jquery para facilitar a seleção de elementos da DOM
--- index.js
+++ index.js
@@ -1,76 +1,35 @@
-const skills = [
- {
-   name: 'HTML 5',
-   color: 'rgb(255, 156, 35)',
- },
- {
-   name: 'CSS 3',
-   color: 'rgb(43, 43, 255)',
- },
- {
-   name: 'NODEJS',
-   color: 'rgb(2, 163, 2)',
- },
- {
-   name: 'NESTJS',
-   color: 'rgb(252, 20, 20)',
- },
- {
-   name: 'MYSQL',
-   color: 'rgb(69, 112, 243)',
- },
- {
-   name: 'MONGODB',
-   color: 'rgb(16, 218, 16)',
- },
- {
-   name: 'RUBY ON RAILS',
-   color: 'rgb(150, 13, 20)',
- },
- {
-   name: 'STRIPE',
-   color: 'rgb(151, 35, 228)',
- },
- {
-   name: 'VUEJS',
-   color: 'rgb(2, 163, 2)',
- },
- {
-   name: 'ANGULARJS',
-   color: 'rgb(252, 20, 20)',
- },
- {
-   name: 'FIREBASE',
-   color: 'rgb(233, 133, 39)',
- },
- {
-   name: 'GIT',
-   color: 'rgb(0, 0, 0)',
- },
-]
-const list = document.getElementById('skills-list')
-
-skills.forEach(skill => {
- const bar = createBar(skill.color)
- const title = createTitle(skill.name, "title")
- const container = document.createElement("li");
- container.append(title) 
- container.append(bar)
- list.appendChild(container)
-})
-
-function createBar(color) {
- const bar = document.createElement("div");
- bar.className = "bar"
- bar.style.backgroundColor = color
- return bar
-}
-
-function createTitle(name) {
- const title = document.createElement("label");
- title.className = "title"
- title.appendChild(document.createTextNode(name))
- return title
-}
-
-document.getElementById("current_year").innerHTML = new Date().getFullYear()
+let p_sobre = $('[name=sobre]').position().top - 68
+let link_sobre = $('[link-sobre]')
+let p_skills = $('[name=skills]').position().top - 220
+let link_skills = $('[link-skills]')
+let p_lab = $('[name=lab]').position().top - 104
+let link_lab = $('[link-lab]')
+let p_ctt = $('[name=ctt]').position().top - 146
+let link_ctt = $('[link-ctt]')
+let style = {'color': 'var(--primary)', 'font-weight': 'bold', 'text-decoration': 'overline'}
+let no_style = {'color': 'var(--text-1)', 'font-weight': '200', 'text-decoration': 'none'}
+setInterval(()=>{
+    let posicao = $(document).scrollTop()
+    if(posicao >= p_sobre && posicao < p_skills){
+        link_sobre.css(style)
+    }else{
+        link_sobre.css(no_style)
+    }
+    if(posicao >= p_skills && posicao < p_lab){
+        link_skills.css(style)
+    }else{
+        link_skills.css(no_style)
+    }
+    if(posicao >= p_lab && posicao < p_ctt){
+        link_lab.css(style)
+    }else{
+        link_lab.css(no_style)
+    }
+    if(posicao >= p_ctt){
+        link_ctt.css(style)
+    }else{
+        link_ctt.css(no_style)
+    }
+}, 100)
+console.log($(document).scrollTop())
+console.log(p_ctt)
